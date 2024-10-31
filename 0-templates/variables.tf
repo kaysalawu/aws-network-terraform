a1776ef3d19dddf8c6a2d888ef9d24d5dfdb1820
Kayode Salawu <salawu@gmail.com>
2024-10-31 17:39:08 +0000

    update

--- 0-templates/variables.tf
+++
@@ -1,4 +0,0 @@
-
-variable "subscription_id" {
-  description = "The Azure subscription ID"
-}
---
+++ 3-labs/a-hybrid/variables.tf
@@ -0,0 +1,16 @@
+
+variable "aws_access_key" {
+  description = "account access key"
+}
+
+variable "aws_secret_access_key" {
+  description = "account secret key"
+}
+
+variable "public_key_path" {
+  description = "path to public key for ec2 SSH"
+}
+
+variable "private_key_path" {
+  description = "path to private key for ec2 SSH"
+}
