ad7855c688857726581a7d4a5cbb36972df785c4
Kayode Salawu <salawu@gmail.com>
2025-04-16 22:11:19 +0000

    fargate added

--- 4-general/g4-netbox-eks-fargate/02-main.tf
+++ 4-general/g4-netbox-eks-fargate/02-main.tf
@@ -57,6 +57,8 @@ data "aws_caller_identity" "current" {
   provider = aws.default
 }
 
+data "aws_availability_zones" "available" {}
+
 ####################################################
 # network features
 ####################################################
