791add8c8537766e4bc6e00370a321b48b1cad05
Kayode Salawu <salawu@gmail.com>
2025-04-17 06:15:11 +0000

    update

--- 4-general/g4-netbox-eks-fargate/svc-hub1-eks-fargate.tf
+++ 4-general/g4-netbox-eks-fargate/svc-hub1-eks-fargate.tf
@@ -4,20 +4,20 @@ locals {
 }
 
 ####################################################
-# EKS Module
+# netbox-eks-fargate
 ####################################################
 
 module "eks" {
-  source = "../../modules/terraform-aws-eks-20.35.0"
+  source = "../../modules/terraform-aws-eks"
 
-  cluster_name    = local.name
+  cluster_name    = "${local.hub1_prefix}eks-fargate"
   cluster_version = local.cluster_version
 
   cluster_endpoint_private_access = true
   cluster_endpoint_public_access  = true
 
   vpc_id = module.hub1.vpc_id
-  subnets = [
+  subnet_ids = [
     module.hub1.subnet_ids["MainSubnetA"],
     module.hub1.subnet_ids["MainSubnetB"],
   ]
@@ -25,54 +25,4 @@ module "eks" {
     module.hub1.subnet_ids["ManagementSubnetA"],
     module.hub1.subnet_ids["ManagementSubnetB"],
   ]
-
-  fargate_profiles = {
-    default = {
-      name = "default"
-      selectors = [
-        {
-          namespace = "kube-system"
-          labels = {
-            k8s-app = "kube-dns"
-          }
-        },
-        {
-          namespace = "default"
-          labels = {
-            WorkerType = "fargate"
-          }
-        }
-      ]
-
-      tags = {
-        Owner = "default"
-      }
-
-      timeouts = {
-        create = "20m"
-        delete = "20m"
-      }
-    }
-
-    secondary = {
-      name = "secondary"
-      selectors = [
-        {
-          namespace = "default"
-          labels = {
-            Environment = "test"
-            GithubRepo  = "terraform-aws-eks"
-            GithubOrg   = "terraform-aws-modules"
-          }
-        }
-      ]
-
-      # Using specific subnets instead of the ones configured in EKS (`subnets` and `fargate_subnets`)
-      subnets = [module.vpc.private_subnets[1]]
-
-      tags = {
-        Owner = "secondary"
-      }
-    }
-  }
 }
